7e2c4706f2e63c485f45829aa980e84ac5bec23d
DX-STAR <139808076+DX-STAR@users.noreply.github.com>
2024-07-21 00:24:25 +0530

    Add files via upload
    This is my portfolio website

---
+++ Devang's Website/script.js
@@ -0,0 +1,7 @@
+//For Typewriting effect
+var typed = new Typed('#element', {
+  strings: ['Engineer', 'Front-End Developer', 'Digital Marketer'],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+});
